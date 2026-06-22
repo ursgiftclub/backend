6529076178440114fc78d9a3886561348d5dec07
ursgiftclub <ursgiftclub@gmail.com>
2026-06-22 16:42:42 +0530

    update login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -123,12 +123,15 @@ export const loginUser = async (req, res) => {
     // Token
     const token = generateToken(user._id);
 
-   res.cookie("token", token, {
-  httpOnly: true,
-  secure: true,
-  sameSite: "none",
-  maxAge: 30 * 24 * 60 * 60 * 1000,
-});
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite:
+        process.env.NODE_ENV === "production"
+          ? "none"
+          : "lax",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
 
     res.status(200).json({
       success: true,
