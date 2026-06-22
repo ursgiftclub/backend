b5b383a3c50235709a959ab18689f222a5a5c559
ursgiftclub <ursgiftclub@gmail.com>
2026-06-22 18:04:50 +0530

    fix reg

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -46,7 +46,10 @@ export const registerUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite:
+        process.env.NODE_ENV === "production"
+          ? "none"
+          : "lax",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
